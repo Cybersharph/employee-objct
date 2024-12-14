7e1f2cc377e9aae1f636a144724c0aaffd101feb
Muyivu Shafiq <cybersharph@gmail.com>
2024-12-14 21:55:07 +0300

    Added retirement eligibility functionality

--- employee.py
+++ employee.py
@@ -4,13 +4,14 @@
 class Employee:
 
     # Constructor method
-    def __init__(self, name, identity, department, role, salary, email):
+    def __init__(self, name, identity, department, role, salary, email, years_in_service):
         self.name = name
         self.identity = identity
         self.department = department
         self.role = role
         self.salary = salary
         self.email = email
+        self.years_in_service = years_in_service
     
     # Method to display employee information
     def employee_info(self):
@@ -21,7 +22,16 @@ class Employee:
         print(f"Role: {self.role}")
         print(f"Salary: {self.salary}")
         print(f"Email address: {self.email}")
+        print(f"Years in service: {self.years_in_service}")
+        self.eligible_for_retirement()
         print("*" * 50)
         print()
+
+    # Methond to check if employee is eligible for retirement
+    def eligible_for_retirement(self):
+        if int(self.years_in_service) >= 10:
+            print("Eligible for retirement.")
+        else:
+            print("Not eligible for retirement.")
 
 # End of Employee class
--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from employee import Employee
 # Create a list of employees.
 # Each employee is an instance of the Employee class.
 employees = [
-  Employee("Muyivu Shafiq", "3341", "IT", "Security Auditor", "100000", "cybersharph@gmail.com"),
-  Employee("Sarah Johnson", "2234", "Finance", "Financial Analyst", "95000", "sarah.johnson@example.com"),
-  Employee("James Williams", "4455", "IT", "Software Developer", "105000", "james.williams@company.com"),
-  Employee("Emma Brown", "6677", "Marketing", "Marketing Coordinator", "88000", "emma.brown@company.com"),
-  Employee("Liam Smith", "8899", "Sales", "Sales Executive", "102000", "liam.smith@sales.com"),
-  Employee("Olivia Davis", "1122", "Operations", "Operations Manager", "110000", "olivia.davis@company.com"),
-  Employee("Noah Miller", "3344", "HR", "Recruitment Specialist", "97000", "noah.miller@hr.com"),
-  Employee("Ava Thompson", "5566", "IT", "Network Engineer", "108000", "ava.thompson@tech.com"),
-  Employee("William Anderson", "7788", "Logistics", "Logistics Supervisor", "95000", "william.anderson@logistics.com")
+    Employee("Muyivu Shafiq", "3341", "IT", "Security Auditor", "100000", "cybersharph@gmail.com" , "5"),
+    Employee("Sarah Johnson", "2234", "Finance", "Financial Analyst", "95000", "sarah.johnson@example.com" , "3"),
+    Employee("James Williams", "4455", "IT", "Software Developer", "105000", "james.williams@company.com", "11"),
+    Employee("Emma Brown", "6677", "Marketing", "Marketing Coordinator", "88000", "emma.brown@company.com", "2"),
+    Employee("Liam Smith", "8899", "Sales", "Sales Executive", "102000", "liam.smith@sales.com", "6"),
+    Employee("Olivia Davis", "1122", "Operations", "Operations Manager", "110000", "olivia.davis@company.com", "12"),
+    Employee("Noah Miller", "3344", "HR", "Recruitment Specialist", "97000", "noah.miller@hr.com", "1"),
+    Employee("Ava Thompson", "5566", "IT", "Network Engineer", "108000", "ava.thompson@tech.com", "8"),
+    Employee("William Anderson", "7788", "Logistics", "Logistics Supervisor", "95000", "william.anderson@logistics.com", "15"),
 ]
 
 # Loop through the list of employee instances.
 for employee in employees:
-  employee.employee_info()
+    employee.employee_info()
